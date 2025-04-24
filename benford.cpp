1184c712f903b1d729981aef3de1225cf9e3e6ba
Mythereus <136450574+MythicArrow@users.noreply.github.com>
2025-04-24 21:57:37 +0200

    Create benford.cpp

---
+++ benford.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+void logFunc(int n){
+  std::cout << log10(1 + 1/n) << std::endl;
+  //Enter a value between 1 and 9 for the percentage distribution.
+ 
+}
+int main() 
+{
+    logFunc(1);
+    return 0;
+}
